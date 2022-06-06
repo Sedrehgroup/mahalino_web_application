24d3e5f746b83db9571d3bb02e67e37b5099886b
Alireza_Dinvarzadeh <mdinvarzade@gmail.com>
2022-06-06 11:19:10 +0430

    change port of geoserver

--- django/mahallino_api/urls.py
+++ django/mahallino_api/urls.py
@@ -1,10 +1,9 @@
 from django.urls import path
-
-from .views import   PerTrans , PubTrans  , Refahi , Security
+from .views import PerTrans, PubTrans, Refahi, Security
 
 urlpatterns = [
-    path('sec', Security.as_view() , name='security'),
-    path('per', PerTrans.as_view() , name='pertrans'),
-    path('pub', PubTrans.as_view() , name='pubtrans'),
-    path('ref', Refahi.as_view() , name='refahi'),
+    path('sec', Security.as_view(), name='security'),
+    path('per', PerTrans.as_view(), name='pertrans'),
+    path('pub', PubTrans.as_view(), name='pubtrans'),
+    path('ref', Refahi.as_view(), name='refahi'),
 ]
